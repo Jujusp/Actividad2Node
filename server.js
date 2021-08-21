609c1b9e5dbcdc42140134b79ed2aa43d441160a
Jujusp <jf.rubio@uniandes.edu.co>
2021-08-21 10:57:07 -0500

    Push Final Funcional

--- server.js
+++ server.js
@@ -1,64 +1,78 @@
 const http = require('http')
 const axios = require('axios')
 const fs = require('fs')
+
 const hostname = '127.0.0.1'
 const port = 8081
 
-const proveedores =
+const proveedoresLink =
     'https://gist.githubusercontent.com/josejbocanegra/d3b26f97573a823a9d0df4ec68fef45f/raw/66440575649e007a9770bcd480badcbbc6a41ba7/proveedores.json'
-const clientes =
+const clientesLink =
     'https://gist.githubusercontent.com/josejbocanegra/986182ce2dd3e6246adcf960f9cda061/raw/f013c156f37c34117c0d4ba9779b15d427fb8dcd/clientes.json'
 
-async function getProveedores() {
+async function fetch(link) {
     try {
-        const response = await axios.get(proveedores)
-        console.log(response)
-        fs.writeFile(
-            'proveedores.json',
-            JSON.stringify(response.data),
-            (err) => {
-                console.log(err)
-            }
-        )
+        const response = await axios.get(link)
+        return response.data
     } catch (error) {
         console.error(error)
     }
 }
 
-async function getClientes() {
-    try {
-        const response = await axios.get(clientes)
-        console.log(response)
-        fs.writeFile('clientes.json', JSON.stringify(response.data), (err) => {
-            console.log(err)
-        })
-    } catch (error) {
-        console.error(error)
-    }
-}
-
-const server = http.createServer((req, res) => {
-    getProveedores()
-    getClientes()
-    console.log(req.url)
+const server = http.createServer(async function (req, res) {
     res.statusCode = 200
-    if (req.url === '/') {
-        res.setHeader('Content-Type', 'text/plain')
-        res.end('Hola Mundo')
-    } else if (req.url === '/proveedores') {
-        fs.readFile('proveedores.json', 'utf-8', (err, data) => {
-            if (err) throw err
-            res.end(data)
-        })
-    } else if (req.url === '/clientes') {
-        fs.readFile('clientes.json', 'utf-8', (err, data) => {
-            if (err) throw err
-            res.end(data)
-        })
-    } else {
-        res.statusCode = 404
-        res.setHeader('Content-Type', 'text/plain')
-        res.end('404 Not Found')
+    switch (req.url) {
+        case '/':
+            res.setHeader('Content-Type', 'text/plain')
+            res.end('Hola Mundo')
+            break
+        case '/api/proveedores':
+            const proveedoresData = await fetch(proveedoresLink)
+            var provHTML = fs.readFileSync('proveedores.html', 'utf-8')
+            res.setHeader('Content-Type', 'text/html')
+            provHTML = provHTML.replace(
+                '?????',
+                proveedoresData.reduce((previousValue, currentValue) => {
+                    return (
+                        previousValue +
+                        '<tr><th scope="row">' +
+                        currentValue.idproveedor +
+                        '</th><td>' +
+                        currentValue.nombrecompania +
+                        '</td><td>' +
+                        currentValue.nombrecontacto +
+                        '</td></tr>'
+                    )
+                }, '')
+            )
+            res.end(provHTML)
+
+            break
+        case '/api/clientes':
+            const clientesData = await fetch(clientesLink)
+            var provHTML = fs.readFileSync('clientes.html', 'utf-8')
+            res.setHeader('Content-Type', 'text/html')
+            provHTML = provHTML.replace(
+                '?????',
+                clientesData.reduce((previousValue, currentValue) => {
+                    return (
+                        previousValue +
+                        '<tr><th scope="row">' +
+                        currentValue.idCliente +
+                        '</th><td>' +
+                        currentValue.NombreCompania +
+                        '</td><td>' +
+                        currentValue.NombreCompania +
+                        '</td></tr>'
+                    )
+                }, '')
+            )
+            res.end(provHTML)
+            break
+        default:
+            res.statusCode = 404
+            res.setHeader('Content-Type', 'text/plain')
+            res.end('404 Not Found')
     }
 })
 
